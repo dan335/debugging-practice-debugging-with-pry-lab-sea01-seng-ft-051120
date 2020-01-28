8293e407a0eb8f1aa8be3c345a20f9a19636cbba
Daniel Phillips <danphi@gmail.com>
2020-01-28 04:51:10 +0000

    Automatically backed up by Learn

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -1,4 +1,4 @@
-
+require "pry"
 
 def get_user_input
   gets.chomp
@@ -11,6 +11,7 @@ def prompt_user
 end
 
 def selection(num)
+  binding.pry
   if num = 1
      "YUM YUM MUNCH MUNCH MUNCH"
   elsif num = 2
